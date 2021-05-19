3cc50c20ca1c941edf79557d672930766a10ec76
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2021-05-19 17:31:39 +0200

    test: simplify test setup
    Use `bootstrapPropertiesPanel` to inject styles, create a bpmn-js
    Modeler instance, and attach the properties panel in the current
    test container.

--- test/TestHelper.js
+++ test/TestHelper.js
@@ -3,7 +3,14 @@ import {
 } from 'min-dom';
 
 import {
+  act,
+  fireEvent
+} from '@testing-library/preact';
+import TestContainer from 'mocha-test-container-support';
+
+import {
   bootstrapBpmnJS,
+  inject,
   insertCSS
 } from 'bpmn-js/test/helper';
 
@@ -11,6 +18,31 @@ import Modeler from 'bpmn-js/lib/Modeler';
 
 export * from 'bpmn-js/test/helper';
 
+export function bootstrapPropertiesPanel(diagram, options, locals) {
+  return async function() {
+    const container = TestContainer.get(this);
+
+    insertBpmnStyles();
+    insertCoreStyles();
+
+    const createModeler = bootstrapBpmnJS(Modeler, diagram, options, locals);
+    await createModeler.call(this);
+
+    const attachPropertiesPanel = inject(function(propertiesPanel) {
+      const propertiesPanelContainer = document.createElement('div');
+      propertiesPanelContainer.classList.add('properties-container');
+
+      container.appendChild(propertiesPanelContainer);
+
+      return act(() => propertiesPanel.attachTo(propertiesPanelContainer));
+    });
+    await attachPropertiesPanel();
+  };
+}
+
+export function changeInput(input, value) {
+  fireEvent.input(input, { target: { value } });
+}
 
 export function insertCoreStyles() {
   insertCSS(
