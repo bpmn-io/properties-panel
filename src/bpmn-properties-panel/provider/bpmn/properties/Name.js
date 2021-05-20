d1935795f88defb1cd5e1294652866a3a4af0f1e
Niklas Kiefer <niklas.kiefer@camunda.com>
2021-05-20 11:12:41 +0200

    feat(bpmn-provider): improve name property
    Related to #9

--- src/bpmn-properties-panel/provider/bpmn/properties/Name.js
+++ src/bpmn-properties-panel/provider/bpmn/properties/Name.js
@@ -1,3 +1,12 @@
+import {
+  getBusinessObject,
+  is
+} from 'bpmn-js/lib/util/ModelUtil';
+
+import {
+  add as collectionAdd
+} from 'diagram-js/lib/util/Collections';
+
 import TextField from '../../../../properties-panel/components/entries/TextField';
 
 import {
@@ -10,28 +19,101 @@ export default function NameProperty(props) {
     element
   } = props;
 
-  // todo(pinussilvestrus): handle other cases as
-  // a) Text Annotations
-  // b) Groups
   const modeling = useService('modeling');
   const debounce = useService('debounceInput');
+  const canvas = useService('canvas');
+  const bpmnFactory = useService('bpmnFactory');
+  const translate = useService('translate');
 
-  const setValue = (value) => {
-    modeling.updateProperties(element, {
-      name: value
-    });
-  };
+  if (is(element, 'bpmn:Collaboration')) {
+    return;
+  }
 
-  const getValue = (element) => {
-    return element.businessObject.name;
-  };
-
-  return TextField({
+  // (1) default: name
+  let options = {
     element,
     id: 'name',
-    label: 'Name',
-    getValue,
-    setValue,
-    debounce
+    label: translate('Name'),
+    debounce,
+    setValue: (value) => {
+      modeling.updateProperties(element, {
+        name: value
+      });
+    },
+    getValue: (element) => {
+      return element.businessObject.name;
+    }
+  };
+
+  // (2) text annotations
+  if (is(element, 'bpmn:TextAnnotation')) {
+    options = {
+      ...options,
+      setValue: (value) => {
+        modeling.updateProperties(element, {
+          text: value
+        });
+      },
+      getValue: (element) => {
+        return element.businessObject.text;
+      }
+    };
+  }
+
+  // (3) groups
+  else if (is(element, 'bpmn:Group')) {
+    options = {
+      ...options,
+      setValue: (value) => {
+        const businessObject = getBusinessObject(element),
+              categoryValueRef = businessObject.categoryValueRef;
+
+        if (!categoryValueRef) {
+          initializeCategory(businessObject, canvas.getRootElement(), bpmnFactory);
+        }
+
+        modeling.updateLabel(element, value);
+      },
+      getValue: (element) => {
+        const businessObject = getBusinessObject(element),
+              categoryValueRef = businessObject.categoryValueRef;
+
+        return categoryValueRef && categoryValueRef.value;
+      }
+    };
+  }
+
+  // (4) participants (only update label)
+  else if (is(element, 'bpmn:Participant')) {
+    options.label = translate('Participant Name');
+  }
+
+
+  return TextField(options);
+}
+
+
+// helpers ////////////////////////
+
+function initializeCategory(businessObject, rootElement, bpmnFactory) {
+  const definitions = getBusinessObject(rootElement).$parent;
+
+  const categoryValue = createCategoryValue(definitions, bpmnFactory);
+
+  businessObject.categoryValueRef = categoryValue;
+}
+
+function createCategoryValue(definitions, bpmnFactory) {
+  const categoryValue = bpmnFactory.create('bpmn:CategoryValue');
+
+  const category = bpmnFactory.create('bpmn:Category', {
+    categoryValue: [ categoryValue ]
   });
+
+  // add to correct place
+  collectionAdd(definitions.get('rootElements'), category);
+  getBusinessObject(category).$parent = definitions;
+  getBusinessObject(categoryValue).$parent = category;
+
+  return categoryValue;
 }
