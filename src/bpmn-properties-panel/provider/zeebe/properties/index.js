5ece7bf052ab9d65513d1780a311aaba0fc67b71
Max Tru <maximilian.trumpf@camunda.com>
2021-05-31 22:16:52 +0200

    feat(zeebe-provider): add target process id for call activites
    closes #39

--- src/bpmn-properties-panel/provider/zeebe/properties/index.js
+++ src/bpmn-properties-panel/provider/zeebe/properties/index.js
@@ -2,4 +2,5 @@ export { default as InputProperties } from './Input';
 export { default as OutputProperties } from './Output';
 export { ConditionProps } from './ConditionProps';
 export { MultiInstanceProps } from './MultiInstanceProps';
+export { TargetProps } from './TargetProps';
 export { TaskDefinitionProps } from './TaskDefinitionProps';
