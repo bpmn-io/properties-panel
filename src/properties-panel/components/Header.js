fcfe090c42e3a53fb29553c9056ba71a54f03618
Niklas Kiefer <niklas.kiefer@camunda.com>
2021-05-31 16:58:31 +0200

    fix(properties-panel): place header icon inside 32x32 viewbox
    Closes #8

--- src/properties-panel/components/Header.js
+++ src/properties-panel/components/Header.js
@@ -26,7 +26,7 @@ export default function Header(props) {
 
   return (<div class="bio-properties-panel-header">
     <div class="bio-properties-panel-header-icon">
-      <ElementIcon width="48" height="48" viewBox="0 0 48 48" />
+      <ElementIcon width="32" height="32" viewBox="0 0 32 32" />
     </div>
     <div class="bio-properties-panel-header-labels">
       { getElementLabel(element) ?
