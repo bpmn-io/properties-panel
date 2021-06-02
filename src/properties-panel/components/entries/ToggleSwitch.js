bfc0e173125b0d146d2767f69c0f7b83302c7475
Max Tru <maximilian.trumpf@camunda.com>
2021-06-02 15:48:39 +0200

    feat(components): add toggleSwitch component

---
+++ src/properties-panel/components/entries/ToggleSwitch.js
@@ -0,0 +1,69 @@
+function ToggleSwitch(props) {
+  const {
+    id,
+    label,
+    onInput,
+    value,
+    switcherLabel
+  } = props;
+
+  const handleInput = async () => {
+    onInput(!value);
+  };
+
+  return (
+    <div class="bio-properties-panel-toggle-switch">
+      <label class="bio-properties-panel-label"
+        for={ prefixId(id) }>
+        { label }
+      </label>
+      <div class="bio-properties-panel-field-wrapper">
+        <label class="bio-properties-panel-toggle-switch__switcher">
+          <input
+            id={ prefixId(id) }
+            type="checkbox"
+            name={ id }
+            onInput={ handleInput }
+            checked={ value } />
+          <span class="bio-properties-panel-toggle-switch__slider" />
+        </label>
+        <p class="bio-properties-panel-toggle-switch__label">{ switcherLabel }</p>
+      </div>
+    </div>
+  );
+}
+
+/**
+ * @param {Object} props
+ * @param {String} props.id
+ * @param {String} props.description
+ * @param {String} props.label
+ * @param {String} props.switcherLabel
+ * @param {Function} props.getValue
+ * @param {Function} props.setValue
+ */
+export default function ToggleSwitchEntry(props) {
+  const {
+    id,
+    description,
+    label,
+    switcherLabel,
+    getValue,
+    setValue
+  } = props;
+
+  const value = getValue();
+  return (
+    <div class="bio-properties-panel-entry" data-entry-id={ id }>
+      <ToggleSwitch id={ id } label={ label } value={ value } onInput={ setValue } switcherLabel={ switcherLabel } />
+      { description && <div class="bio-properties-panel-description">{ description }</div> }
+    </div>
+  );
+}
+
+
+// helpers /////////////////
+
+function prefixId(id) {
+  return `bio-properties-panel-${ id }`;
+}
