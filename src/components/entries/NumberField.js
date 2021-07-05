d6a7661629927295fcd0def04d5654490416a5ec
Niklas Kiefer <niklas.kiefer@camunda.com>
2021-07-05 15:58:42 +0200

    feat(entries): add number field component

---
+++ src/components/entries/NumberField.js
@@ -0,0 +1,104 @@
+import {
+  useMemo
+} from 'preact/hooks';
+
+function NumberField(props) {
+
+  const {
+    debounce,
+    disabled,
+    id,
+    label,
+    max,
+    min,
+    onInput,
+    value = ''
+  } = props;
+
+  const handleInput = useMemo(() => {
+    return debounce(event => {
+
+      const {
+        validity,
+        value
+      } = event.target;
+
+      if (validity.valid) {
+        onInput(value ? parseInt(value, 10) : undefined);
+      }
+    });
+  }, [ onInput, debounce ]);
+
+  return (
+    <div class="bio-properties-panel-numberfield">
+      <label for={ prefixId(id) } class="bio-properties-panel-label">{ label }</label>
+      <input
+        id={ prefixId(id) }
+        type="number"
+        name={ id }
+        spellCheck="false"
+        autoComplete="off"
+        disabled={ disabled }
+        class="bio-properties-panel-input"
+        max={ max }
+        min={ min }
+        onInput={ handleInput }
+        value={ value } />
+    </div>
+  );
+}
+
+/**
+ * @param {Object} props
+ * @param {Boolean} props.debounce
+ * @param {String} props.description
+ * @param {Boolean} props.disabled
+ * @param {Object} props.element
+ * @param {Function} props.getValue
+ * @param {String} props.id
+ * @param {String} props.label
+ * @param {String} props.max
+ * @param {String} props.min
+ * @param {Function} props.setValue
+ */
+export default function NumberFieldEntry(props) {
+  const {
+    debounce,
+    description,
+    disabled,
+    element,
+    getValue,
+    id,
+    label,
+    max,
+    min,
+    setValue
+  } = props;
+
+  const value = getValue(element);
+  return (
+    <div class="bio-properties-panel-entry" data-entry-id={ id }>
+      <NumberField
+        debounce={ debounce }
+        disabled={ disabled }
+        id={ id }
+        label={ label }
+        onInput={ setValue }
+        max={ max }
+        min={ min }
+        value={ value } />
+      { description && <div class="bio-properties-panel-description">{ description }</div> }
+    </div>
+  );
+}
+
+export function isEdited(node) {
+  return node && !!node.value;
+}
+
+
+// helpers /////////////////
+
+function prefixId(id) {
+  return `bio-properties-panel-${ id }`;
+}
---
+++ test/spec/components/NumberField.spec.js
@@ -0,0 +1,189 @@
+import {
+  render
+} from '@testing-library/preact/pure';
+
+import TestContainer from 'mocha-test-container-support';
+
+import {
+  query as domQuery
+} from 'min-dom';
+
+import {
+  insertCoreStyles,
+  changeInput
+} from 'test/TestHelper';
+
+import NumberField, { isEdited } from 'src/components/entries/NumberField';
+
+insertCoreStyles();
+
+const noop = () => {};
+
+
+describe('<NumberField>', function() {
+
+  let container;
+
+  beforeEach(function() {
+    container = TestContainer.get(this);
+  });
+
+
+  it('should render', function() {
+
+    // given
+    const result = createNumberField({ container });
+
+    // then
+    expect(domQuery('.bio-properties-panel-numberfield', result.container)).to.exist;
+  });
+
+
+  it('should update', function() {
+
+    // given
+    const updateSpy = sinon.spy();
+
+    const result = createNumberField({ container, setValue: updateSpy });
+
+    const input = domQuery('.bio-properties-panel-input', result.container);
+
+    // when
+    changeInput(input, 20);
+
+    // then
+    expect(updateSpy).to.have.been.calledWith(20);
+  });
+
+
+  describe('#isEdited', function() {
+
+    it('should NOT be edited', function() {
+
+      // given
+      const result = createNumberField({ container });
+
+      const input = domQuery('.bio-properties-panel-input', result.container);
+
+      // when
+      const edited = isEdited(input);
+
+      // then
+      expect(edited).to.be.false;
+    });
+
+
+    it('should be edited', function() {
+
+      // given
+      const result = createNumberField({ container, getValue: () => 20 });
+
+      const input = domQuery('.bio-properties-panel-input', result.container);
+
+      // when
+      const edited = isEdited(input);
+
+      // then
+      expect(edited).to.be.true;
+    });
+
+
+    it('should be edited after update', function() {
+
+      // given
+      const result = createNumberField({ container });
+
+      const input = domQuery('.bio-properties-panel-input', result.container);
+
+      // assume
+      expect(isEdited(input)).to.be.false;
+
+      // when
+      changeInput(input, 20);
+
+      // then
+      expect(isEdited(input)).to.be.true;
+    });
+
+  });
+
+
+  describe('disabled', function() {
+
+    it('should render enabled per default', function() {
+
+      // given
+      const result = createNumberField({ container });
+
+      // then
+      const input = domQuery('.bio-properties-panel-input', result.container);
+      expect(input.disabled).to.be.false;
+    });
+
+
+    it('should render enabled if set', function() {
+
+      // given
+      const result = createNumberField({
+        container,
+        disabled: false
+      });
+
+      // then
+      const input = domQuery('.bio-properties-panel-input', result.container);
+      expect(input.disabled).to.be.false;
+    });
+
+
+    it('should render disabled if set', function() {
+
+      // given
+      const result = createNumberField({
+        container,
+        disabled: true
+      });
+
+      // then
+      const input = domQuery('.bio-properties-panel-input', result.container);
+      expect(input.disabled).to.be.true;
+    });
+
+  });
+
+});
+
+
+// helpers ////////////////////
+
+function createNumberField(options = {}) {
+  const {
+    element,
+    debounce = fn => fn,
+    description,
+    disabled,
+    getValue = noop,
+    id,
+    label,
+    max,
+    min,
+    setValue = noop,
+    container
+  } = options;
+
+  return render(
+    <NumberField
+      element={ element }
+      debounce={ debounce }
+      description={ description }
+      disabled={ disabled }
+      getValue={ getValue }
+      id={ id }
+      label={ label }
+      max={ max }
+      min={ min }
+      setValue={ setValue } />,
+    {
+      container
+    }
+  );
+}
