d9a6c3b8e4b4643b342d8f89f4504ed01056b53b
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2021-08-31 19:48:52 +0200

    feat(hooks): add useKeyFactory
    The hook allows to persist keys between renders
    for items which don't have ids on their own.

---
+++ src/hooks/useKeyFactory.js
@@ -0,0 +1,39 @@
+import { useMemo } from 'preact/hooks';
+
+const KEY_LENGTH = 6;
+
+/**
+ * Create a persistent key factory for plain objects without id.
+ *
+ * @example
+ * ```jsx
+ * function List({ objects }) {
+ *   const getKey = useKeyFactory();
+ *   return (<ol>{
+ *     objects.map(obj => {
+ *       const key = getKey(obj);
+ *       return <li key={key}>obj.name</li>
+ *     })
+ *   }</ol>);
+ * }
+ * ```
+ *
+ * @param {any[]} dependencies
+ * @returns {(element: object) => string}
+ */
+export function useKeyFactory(dependencies = []) {
+  const map = useMemo(() => new WeakMap(), dependencies);
+
+  const getKey = el => {
+    let key = map.get(el);
+
+    if (!key) {
+      key = Math.random().toString().slice(-KEY_LENGTH);
+      map.set(el, key);
+    }
+
+    return key;
+  };
+
+  return getKey;
+}
