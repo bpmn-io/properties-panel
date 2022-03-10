f11adcc2b9cfb3d8ebc9265d904280bcc37c6763
Martin Stamm <martin.stamm@camunda.com>
2022-03-10 09:13:45 +0100

    feat(TextField): allow addition of FEEL icon
    related to https://github.com/bpmn-io/bpmn-js-properties-panel/issues/599

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,3 +1,5 @@
 export { default as ArrowIcon } from './Arrow.svg';
 export { default as CreateIcon } from './Create.svg';
 export { default as DeleteIcon } from './Delete.svg';
+export { default as FeelRequiredIcon } from './FeelRequired.svg';
+export { default as FeelOptionalIcon } from './FeelOptional.svg';
--- test/spec/components/TextField.spec.js
+++ test/spec/components/TextField.spec.js
@@ -336,6 +336,57 @@ describe('<TextField>', function() {
   });
 
 
+  describe('feel', function() {
+
+    it('should render without feel icon', function() {
+
+      // given
+      const result = createTextField({
+        container,
+        id: 'noFeelTextField'
+      });
+
+      // then
+      const icon = domQuery('[data-entry-id="noFeelTextField"] .bio-properties-panel-feel-icon',
+        result.container);
+      expect(icon).not.to.exist;
+    });
+
+
+    it('should render optional feel icon', function() {
+
+      // given
+      const result = createTextField({
+        container,
+        id: 'requiredFeelTextField',
+        feel: 'optional'
+      });
+
+      // then
+      const icon = domQuery('[data-entry-id="requiredFeelTextField"] .bio-properties-panel-feel-icon',
+        result.container);
+      expect(icon).to.exist;
+    });
+
+
+    it('should render required feel icon', function() {
+
+      // given
+      const result = createTextField({
+        container,
+        id: 'requiredFeelTextField',
+        feel: 'required'
+      });
+
+      // then
+      const icon = domQuery('[data-entry-id="requiredFeelTextField"] .bio-properties-panel-feel-icon',
+        result.container);
+      expect(icon).to.exist;
+    });
+
+  });
+
+
   describe('a11y', function() {
 
     it('should have no violations', async function() {
@@ -364,6 +415,7 @@ function createTextField(options = {}) {
     description,
     debounce = fn => fn,
     disabled,
+    feel,
     label,
     getValue = noop,
     setValue = noop,
@@ -389,7 +441,8 @@ function createTextField(options = {}) {
         getValue={ getValue }
         setValue={ setValue }
         debounce={ debounce }
-        validate={ validate } />
+        validate={ validate }
+        feel={ feel } />
     </DescriptionContext.Provider>,
     {
       container
