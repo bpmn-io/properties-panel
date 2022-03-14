7ba494a42edd572ba578b62e6ce8692aa723bb18
Martin Stamm <martin.stamm@camunda.com>
2022-03-14 17:08:33 +0100

    feat(TextArea): allow addition of FEEL icon

--- src/components/entries/TextField.js
+++ src/components/entries/TextField.js
@@ -1,4 +1,5 @@
 import Description from './Description';
+import FeelIcon from './FeelIcon';
 
 import {
   useEffect,
@@ -11,32 +12,6 @@ import classnames from 'classnames';
 import {
   usePrevious
 } from '../../hooks';
-import { FeelOptionalIcon, FeelRequiredIcon } from '../icons';
-
-
-function FeelIcon(props) {
-
-  const {
-    label,
-    feel = false,
-  } = props;
-
-  const feelRequiredLabel = ' must be a FEEL expression';
-  const feelOptionalLabel = ' can optionally be a FEEL expression';
-
-  return (
-    <i
-      class="bio-properties-panel-feel-icon"
-      title={
-        label + (
-          feel === 'required' ? feelRequiredLabel : feelOptionalLabel
-        )
-      }
-    >
-      {feel === 'required' ? <FeelRequiredIcon /> : <FeelOptionalIcon />}
-    </i>
-  );
-}
 
 
 function Textfield(props) {
