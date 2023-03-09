b383a0c88f421af7b73a4162e0621b7d12f5e0c5
Valentin Serra <valentin.serra@camunda.com>
2023-03-09 05:48:46 +0100

    feat: feelers editor
    Related to [team-hto/141](https://github.com/camunda/team-hto/issues/141)

---
+++ src/components/entries/templating/FeelTemplating.js
@@ -0,0 +1,206 @@
+import { useCallback, useEffect, useState, useMemo, useRef } from 'preact/hooks';
+import { usePrevious, useStaticCallback, useShowEntryEvent } from '../../../hooks';
+import { isFunction } from 'min-dash';
+import { useError } from '../../../hooks';
+import classnames from 'classnames';
+import Description from '../Description';
+import FeelTemplatingEditor from './FeelTemplatingEditor';
+
+const noop = () => { };
+
+/**
+ * @param {Object} props
+ * @param {Object} props.element
+ * @param {String} props.id
+ * @param {String} props.description
+ * @param {Boolean} props.debounce
+ * @param {Boolean} props.disabled
+ * @param {String} props.label
+ * @param {Function} props.getValue
+ * @param {Function} props.setValue
+ * @param {Function} props.validate
+ */
+export default function FeelTemplatingEntry(props) {
+  const {
+    element,
+    id,
+    description,
+    debounce,
+    disabled,
+    label,
+    getValue,
+    setValue,
+    tooltipContainer,
+    validate,
+    show = noop,
+  } = props;
+
+  const [ cachedInvalidValue, setCachedInvalidValue ] = useState(null);
+  const [ validationError, setValidationError ] = useState(null);
+  const [ localError, setLocalError ] = useState(null);
+
+  let value = getValue(element);
+
+  const previousValue = usePrevious(value);
+
+  useEffect(() => {
+    if (isFunction(validate)) {
+      const newValidationError = validate(value) || null;
+
+      setValidationError(newValidationError);
+    }
+  }, [ value ]);
+
+  const onInput = useStaticCallback((newValue) => {
+    let newValidationError = null;
+
+    if (isFunction(validate)) {
+      newValidationError = validate(newValue) || null;
+    }
+
+    if (newValidationError) {
+      setCachedInvalidValue(newValue);
+    } else {
+
+      // don't create multiple commandStack entries for the same value
+      if (newValue !== value) {
+        setValue(newValue);
+      }
+    }
+
+    setValidationError(newValidationError);
+  });
+
+  const onError = useCallback(err => {
+    setLocalError(err);
+  }, []);
+
+  if (previousValue === value && validationError) {
+    value = cachedInvalidValue;
+  }
+
+  const temporaryError = useError(id);
+
+  const error = localError || temporaryError || validationError;
+
+  return (
+    <div
+      class={ classnames(
+        'bio-properties-panel-entry',
+        error ? 'has-error' : '')
+      }
+      data-entry-id={ id }>
+      <FeelTemplating
+        debounce={ debounce }
+        disabled={ disabled }
+        id={ id }
+        key={ element }
+        label={ label }
+        onInput={ onInput }
+        onError={ onError }
+        show={ show }
+        value={ value }
+        tooltipContainer={ tooltipContainer } />
+      {error && <div class="bio-properties-panel-error">{error}</div>}
+      <Description forId={ id } element={ element } value={ description } />
+    </div>
+  );
+}
+
+function FeelTemplating(props) {
+  const {
+    debounce,
+    id,
+    label,
+    onInput,
+    onError,
+    value = '',
+    disabled = false,
+    tooltipContainer
+  } = props;
+
+  const [ localValue, setLocalValue ] = useState(value);
+
+  const editorRef = useShowEntryEvent(id);
+  const containerRef = useRef();
+
+  const [ focus, _setFocus ] = useState(undefined);
+
+  const setFocus = (offset = 0) => {
+    const hasFocus = containerRef.current.contains(document.activeElement);
+
+    // Keep caret position if it is already focused, otherwise focus at the end
+    const position = hasFocus ? document.activeElement.selectionStart : Infinity;
+
+    _setFocus(position + offset);
+  };
+
+  const handleInputCallback = useMemo(() => {
+    return debounce((newValue) => onInput(newValue.length ? newValue : undefined));
+  }, [ onInput, debounce ]);
+
+  const handleInput = newValue => {
+    handleInputCallback(newValue);
+    setLocalValue(newValue);
+  };
+
+  const handleLint = useStaticCallback(lint => {
+
+    const errors = lint && lint.length && lint.filter(e => e.severity === 'error') || [];
+
+    if (!errors.length) {
+      onError(undefined);
+      return;
+    }
+
+    const error = lint[0];
+    const message = `${error.source}: ${error.message}`;
+
+    onError(message);
+  });
+
+  useEffect(() => {
+    if (typeof focus !== 'undefined') {
+      editorRef.current.focus(focus);
+      _setFocus(undefined);
+    }
+  }, [ focus ]);
+
+  useEffect(() => {
+    if (value === localValue) {
+      return;
+    }
+    setLocalValue(value ? value : '');
+  }, [ value ]);
+
+  return (
+    <div class="bio-properties-panel-feelers">
+      <label id={ prefixIdLabel(id) } class="bio-properties-panel-label" onClick={ () => setFocus() }>
+        { label }
+      </label>
+      <div class="bio-properties-panel-feelers-input" ref={ containerRef }>
+        <FeelTemplatingEditor
+          name={ id }
+          onInput={ handleInput }
+          contentAttributes={ { 'aria-labelledby' : prefixIdLabel(id) } }
+          disabled={ disabled }
+          onLint={ handleLint }
+          value={ localValue }
+          useGutters={ false }
+          ref={ editorRef }
+          tooltipContainer={ tooltipContainer }
+        />
+      </div>
+    </div>
+  );
+}
+
+export function isEdited(node) {
+  return node && (!!node.value || node.classList.contains('edited'));
+}
+
+// helpers /////////////////
+
+function prefixIdLabel(id) {
+  return `bio-properties-panel-feelers-${id}-label`;
+}
---
+++ test/spec/components/FeelTemplating.spec.js
@@ -0,0 +1,619 @@
+import { act } from 'preact/test-utils';
+
+import { waitFor } from '@testing-library/dom';
+import {
+  render
+} from '@testing-library/preact/pure';
+
+import TestContainer from 'mocha-test-container-support';
+
+import {
+  classes as domClasses,
+  query as domQuery
+} from 'min-dom';
+
+import EventBus from 'diagram-js/lib/core/EventBus';
+
+import {
+  expectNoViolations,
+  insertCoreStyles
+} from 'test/TestHelper';
+
+import {
+  DescriptionContext,
+  ErrorsContext,
+  EventContext,
+  PropertiesPanelContext
+} from 'src/context';
+
+import FeelTemplatingEntry, { isEdited } from 'src/components/entries/templating/FeelTemplating.js';
+
+insertCoreStyles();
+
+const noop = () => {};
+
+describe('<FeelTemplating>', function() {
+
+  let container;
+  let clock;
+
+  beforeEach(function() {
+    container = TestContainer.get(this);
+    clock = sinon.useFakeTimers();
+  });
+
+  afterEach(function() {
+    clock.restore();
+  });
+
+  describe('Templating Editor', function() {
+
+    it('should render', function() {
+
+      // given
+      const result = createTemplatingEntry({ container });
+
+      // then
+      expect(domQuery('.bio-properties-panel-entry > .bio-properties-panel-feelers > .bio-properties-panel-feelers-input', result.container)).to.exist;
+    });
+
+
+    it('should update', async function() {
+
+      // given
+      const updateSpy = sinon.spy();
+
+      const result = createTemplatingEntry({ container, setValue: updateSpy });
+
+      const input = domQuery('[role="textbox"]', result.container);
+
+      // when
+      await act(() => input.textContent = 'Template {{x}}');
+
+      // then
+      return await waitFor(() => {
+        expect(updateSpy).to.have.been.calledWith('Template {{x}}');
+      });
+    });
+
+
+    describe('#isEdited', function() {
+
+      it('should NOT be edited', function() {
+
+        // given
+        const result = createTemplatingEntry({ container });
+
+        const input = domQuery('.bio-properties-panel-input', result.container);
+
+        // when
+        const edited = isEdited(input);
+
+        // then
+        expect(edited).to.be.false;
+      });
+
+
+      it('should be edited', function() {
+
+        // given
+        const result = createTemplatingEntry({ container, getValue: () => 'foo' });
+
+        const input = domQuery('.bio-properties-panel-input', result.container);
+
+        // when
+        const edited = isEdited(input);
+
+        // then
+        expect(edited).to.be.true;
+      });
+
+
+      it('should be edited after update', async function() {
+
+        // given
+        const result = createTemplatingEntry({ container });
+
+        const input = domQuery('.bio-properties-panel-input', result.container);
+        const contentEditable = domQuery('[role="textbox"]', result.container);
+
+        // assume
+        expect(isEdited(input)).to.be.false;
+
+        // when
+        await act(() => contentEditable.textContent = 'foo');
+
+        // then
+        await waitFor(() => {
+          expect(isEdited(input)).to.be.true;
+        });
+      });
+
+    });
+
+
+    describe('events', function() {
+
+      it('should show entry', function() {
+
+        // given
+        const eventBus = new EventBus();
+
+        const onShowSpy = sinon.spy();
+
+
+        createTemplatingEntry({ id: 'foo', container, eventBus, onShow: onShowSpy });
+
+        // when
+        act(() => eventBus.fire('propertiesPanel.showEntry', { id: 'foo' }));
+
+        // then
+        expect(onShowSpy).to.have.been.called;
+      });
+
+    });
+
+
+    describe('errors', function() {
+
+      it('should get error', function() {
+
+        // given
+        const errors = {
+          foo: 'bar'
+        };
+
+        const result = createTemplatingEntry({ container, errors, id: 'foo' });
+
+        // then
+        expect(domQuery('.bio-properties-panel-error', result.container)).to.exist;
+      });
+
+
+      it('should show syntax error (FEEL)', async function() {
+
+        // given
+        let result = createTemplatingEntry({ container, getValue: () => '= foo == bar' });
+
+        // when
+        await act(() => clock.tick(1000));
+        await act(() => { clock.restore(); });
+
+        // then
+        await waitFor(() => {
+          expect(domQuery('.bio-properties-panel-error', result.container)).to.exist;
+        });
+
+      });
+
+
+      it('should show syntax error (feelers)', async function() {
+
+        // given
+        const result = createTemplatingEntry({ container, getValue: () => 'Template {{foo == bar}}' });
+
+        // when
+        // trigger debounced validation
+        await act(() => clock.tick(1000));
+        await act(() => { clock.restore(); });
+
+        // then
+        await waitFor(() => {
+          expect(domQuery('.bio-properties-panel-error', result.container)).to.exist;
+        });
+
+      });
+
+
+      it('should not show syntax warnings', function() {
+
+        // given
+        const result = createTemplatingEntry({ container, getValue: () => 'Template {{}}' });
+
+        // when
+        // trigger debounced validation
+        clock.tick(1000);
+
+        // then
+        return waitFor(() => {
+          expect(domQuery('.bio-properties-panel-error', result.container)).to.not.exist;
+        });
+
+      });
+
+
+      it('should show local error over global error', async function() {
+
+        // given
+        const errors = {
+          foo: 'bar'
+        };
+
+        const result = createTemplatingEntry({
+          id: 'foo',
+          container,
+          errors,
+          getValue: () => '= foo == bar'
+        });
+
+        // assume
+        let errorField = domQuery('.bio-properties-panel-error', result.container);
+        expect(errorField).to.exist;
+        expect(errorField.textContent).to.eql('bar');
+
+        // when
+        // trigger debounced validation
+        await act(() => { clock.tick(1000); });
+        await act(() => { clock.restore(); });
+
+        // then
+        await waitFor(() => {
+          errorField = domQuery('.bio-properties-panel-error', result.container);
+          expect(errorField).to.exist;
+          expect(errorField.textContent).not.to.eql('bar');
+        });
+      });
+
+
+      it('should not show local warning over global error', async function() {
+
+        // given
+        const errors = {
+          foo: 'bar'
+        };
+
+        const result = createTemplatingEntry({
+          id: 'foo',
+          container,
+          errors,
+          getValue: () => '{{}}'
+        });
+
+        // assume
+        let errorField = domQuery('.bio-properties-panel-error', result.container);
+        expect(errorField).to.exist;
+        expect(errorField.textContent).to.eql('bar');
+
+        // when
+        // trigger debounced validation
+        await act(() => { clock.tick(1000); });
+        await act(() => { clock.restore(); });
+
+        // then
+        await waitFor(() => {
+          errorField = domQuery('.bio-properties-panel-error', result.container);
+          expect(errorField).to.exist;
+          expect(errorField.textContent).to.eql('bar');
+        });
+      });
+
+    });
+
+
+    describe('validation', function() {
+
+      it('should set valid', function() {
+
+        // given
+        const validate = (v) => {
+          if (v === '{{bar}}') {
+            return 'error';
+          }
+        };
+
+        const result = createTemplatingEntry({ container, validate });
+
+        const entry = domQuery('.bio-properties-panel-entry', result.container);
+
+        const input = domQuery('[role="textbox"]', entry);
+
+        // when
+        input.textContent = '{{foo}}';
+
+        // then
+        return waitFor(() => {
+          expect(isValid(entry)).to.be.true;
+        });
+      });
+
+
+      it('should set invalid', async function() {
+
+        // given
+        const validate = (v) => {
+          if (v === '{{bar}}') {
+            return 'error';
+          }
+        };
+
+        const result = createTemplatingEntry({ container, validate });
+
+        const entry = domQuery('.bio-properties-panel-entry', result.container);
+        const input = domQuery('[role="textbox"]', entry);
+
+        // when
+        await act(() => input.textContent = '{{bar}}');
+
+        // then
+        return await waitFor(() => {
+          expect(isValid(entry)).to.be.false;
+        });
+      });
+
+
+      it('should keep showing invalid value', function() {
+
+        // given
+        const validate = (v) => {
+          if (v === '{{bar}}') {
+            return 'error';
+          }
+        };
+
+        const result = createTemplatingEntry({ container, validate });
+
+        const entry = domQuery('.bio-properties-panel-entry', result.container);
+        const input = domQuery('[role="textbox"]', entry);
+
+        // when
+        input.textContent = '{{bar}}';
+
+        // then
+        return waitFor(() => {
+          expect(input.textContent).to.eql('{{bar}}');
+        });
+      });
+
+
+      it('should show error message', async function() {
+
+        // given
+        const validate = (v) => {
+          if (v === 'bar') {
+            return 'error';
+          }
+        };
+
+        const result = createTemplatingEntry({ container, validate });
+
+        const entry = domQuery('.bio-properties-panel-entry', result.container);
+        const input = domQuery('[role="textbox"]', entry);
+
+        // when
+        await act(() => input.textContent = 'bar');
+
+        // then
+        await waitFor(() => {
+          const error = domQuery('.bio-properties-panel-error', entry);
+          expect(error).to.exist;
+          expect(error.innerText).to.eql('error');
+        });
+      });
+
+    });
+
+
+    describe('disabled', function() {
+
+      it('should render enabled per default', function() {
+
+        // given
+        const result = createTemplatingEntry({ container });
+
+        // then
+        const editorContainer = domQuery('.bio-properties-panel-feelers-editor', result.container);
+        expect(editorContainer.classList.contains('disabled')).to.be.false;
+      });
+
+
+      it('should render enabled if set', function() {
+
+        // given
+        const result = createTemplatingEntry({
+          container,
+          disabled: false
+        });
+
+        // then
+        const editorContainer = domQuery('.bio-properties-panel-feelers-editor', result.container);
+        expect(editorContainer.classList.contains('disabled')).to.be.false;
+      });
+
+
+      it('should render disabled if set', function() {
+
+        // given
+        const result = createTemplatingEntry({
+          container,
+          disabled: true
+        });
+
+        // then
+        const editorContainer = domQuery('.bio-properties-panel-feelers-editor', result.container);
+        expect(editorContainer.classList.contains('disabled')).to.be.true;
+      });
+
+    });
+
+
+    describe('description', function() {
+
+      it('should render without description per default', function() {
+
+        // given
+        const result = createTemplatingEntry({
+          container,
+          id: 'noDescriptionTextField'
+        });
+
+        // then
+        const description = domQuery('[data-entry-id="noDescriptionTextField"] .bio-properties-panel-description',
+          result.container);
+        expect(description).not.to.exist;
+      });
+
+
+      it('should render with description if set per props', function() {
+
+        // given
+        const result = createTemplatingEntry({
+          container,
+          id: 'descriptionTextField',
+          label: 'someLabel',
+          description: 'my description'
+        });
+
+        // then
+        const description = domQuery('[data-entry-id="descriptionTextField"] .bio-properties-panel-description',
+          result.container);
+
+        expect(description).to.exist;
+        expect(description.innerText).to.equal('my description');
+      });
+
+
+      it('should render with description if set per context', function() {
+
+        // given
+        const descriptionConfig = { descriptionTextField: (element) => 'myContextDesc' };
+
+        const result = createTemplatingEntry({
+          container,
+          id: 'descriptionTextField',
+          label: 'someLabel',
+          descriptionConfig,
+          getDescriptionForId: (id, element) => descriptionConfig[id](element)
+        });
+
+        // then
+        const description = domQuery('[data-entry-id="descriptionTextField"] .bio-properties-panel-description',
+          result.container);
+
+        expect(description).to.exist;
+        expect(description.innerText).to.equal('myContextDesc');
+      });
+
+
+      it('should render description set per props over context', function() {
+
+        // given
+        const descriptionConfig = { descriptionTextField: (element) => 'myContextDesc' };
+
+        const result = createTemplatingEntry({
+          container,
+          id: 'descriptionTextField',
+          label: 'someLabel',
+          description: 'myExplicitDescription',
+          descriptionConfig,
+          getDescriptionForId: (id, element) => descriptionConfig[id](element)
+        });
+
+        // then
+        const description = domQuery('[data-entry-id="descriptionTextField"] .bio-properties-panel-description',
+          result.container);
+
+        expect(description).to.exist;
+        expect(description.innerText).to.equal('myExplicitDescription');
+      });
+
+    });
+
+  });
+
+
+  describe('a11y', function() {
+
+    it('should have no violations', async function() {
+
+      // given
+      this.timeout(5000);
+
+      const { container: node } = createTemplatingEntry({
+        id: 'foo',
+        container,
+        label: 'foo'
+      });
+
+      await act(() => { clock.tick(1000); });
+      await act(() => { clock.restore(); });
+
+      // then
+      await waitFor(async () => {
+        await expectNoViolations(node);
+      });
+
+    });
+
+  });
+
+});
+
+
+// helpers ////////////////////
+
+function createTemplatingEntry(options = {}, renderFn = render) {
+  const {
+    element,
+    id,
+    description,
+    debounce = fn => fn,
+    disabled,
+    label,
+    getValue = noop,
+    setValue = noop,
+    validate = noop,
+    descriptionConfig = {},
+    getDescriptionForId = noop,
+    container,
+    eventBus = new EventBus(),
+    onShow = noop,
+    errors = {},
+  } = options;
+
+  const errorsContext = {
+    errors
+  };
+
+  const eventContext = {
+    eventBus
+  };
+
+  const propertiesPanelContext = {
+    onShow
+  };
+
+  const descriptionContext = {
+    description: descriptionConfig,
+    getDescriptionForId
+  };
+
+  return renderFn(
+    <ErrorsContext.Provider value={ errorsContext }>
+      <EventContext.Provider value={ eventContext }>
+        <PropertiesPanelContext.Provider value={ propertiesPanelContext }>
+          <DescriptionContext.Provider value={ descriptionContext }>
+            <FeelTemplatingEntry
+              element={ element }
+              id={ id }
+              description={ description }
+              debounce={ debounce }
+              disabled={ disabled }
+              label={ label }
+              getValue={ getValue }
+              setValue={ setValue }
+              tooltipContainer={ container }
+              validate={ validate }
+            />
+          </DescriptionContext.Provider>
+        </PropertiesPanelContext.Provider>
+      </EventContext.Provider>
+    </ErrorsContext.Provider>,
+    {
+      container
+    }
+  );
+}
+
+function isValid(node) {
+  return !domClasses(node).has('has-error');
+}
