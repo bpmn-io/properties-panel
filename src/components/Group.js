47bd89a942facfbf6df7262fa0c3c1d8218264f4
Beatriz Mendes <bea.smendes98@gmail.com>
2023-07-28 15:17:07 +0100

    fix(tooltip): render outside group header
    Closes #266

--- src/components/Group.js
+++ src/components/Group.js
@@ -98,7 +98,7 @@ export default function Group(props) {
       (sticky && open) ? 'sticky' : ''
     ) } onClick={ toggleOpen }>
       <div title={ label } class="bio-properties-panel-group-header-title">
-        <Tooltip value={ props.tooltip } forId={ 'group-' + id } element={ element }>
+        <Tooltip value={ props.tooltip } forId={ 'group-' + id } element={ element } parent={ groupRef }>
           { label }
         </Tooltip>
       </div>
