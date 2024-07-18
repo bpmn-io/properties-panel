bdf8f13a5453db0cf4b61ed4b2bb6e51f55dae02
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2024-07-18 17:27:30 +0200

    chore: simplify code

--- src/hooks/useElementVisible.js
+++ src/hooks/useElementVisible.js
@@ -2,27 +2,22 @@ import { useLayoutEffect, useState } from 'preact/hooks';
 
 export function useElementVisible(element) {
 
-  const [ visible, setVisible ] = useState(false);
+  const [ visible, setVisible ] = useState(!!element && !!element.clientHeight);
 
   useLayoutEffect(() => {
     if (!element) return;
 
     const resizeObserver = new ResizeObserver(([ entry ]) => {
-
-      // Prevents ResizeObserver from going into infinite loop.
-      requestAnimationFrame(() => {
-
-        const newVisible = !!entry.contentRect.height;
-        if (newVisible !== visible) {
-          setVisible(newVisible);
-        }
-      });
+      const newVisible = !!entry.contentRect.height;
+      if (newVisible !== visible) {
+        setVisible(newVisible);
+      }
     });
 
     resizeObserver.observe(element);
 
     return () => resizeObserver.disconnect();
-  }, [ element ]);
+  }, [ element, visible ]);
 
   return visible;
 }
