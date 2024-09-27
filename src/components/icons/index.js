b31dbb061921b8d7b5f15d6dcfa1918b588f2520
Philipp Fromme <philipp.fromme@camunda.com>
2024-09-27 11:31:14 +0200

    feat: make FEEL popup links configurable
    * removes existing links
    * links to be added through configuration instead

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,9 +1,9 @@
 export { default as ArrowIcon } from './Arrow.svg';
+export { default as CloseIcon } from './Close.svg';
 export { default as CreateIcon } from './Create.svg';
 export { default as DeleteIcon } from './Delete.svg';
 export { default as DragIcon } from './Drag.svg';
 export { default as ExternalLinkIcon } from './ExternalLink.svg';
 export { default as FeelIcon } from './Feel.svg';
-export { default as HelpIcon } from './help.svg';
+export { default as LaunchIcon } from './Launch.svg';
 export { default as PopupIcon } from './Popup.svg';
-export { default as CloseIcon } from './Close.svg';
